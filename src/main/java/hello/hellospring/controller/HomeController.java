e4bbe8b576b8445a661d6f78bd0fc205ee203f73
jerry(company) <wildmonk1025@gmail.com>
2022-12-06 17:55:17 +0900

    etc

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -1,6 +1,5 @@
 package hello.hellospring.controller;
 
-import hello.hellospring.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
